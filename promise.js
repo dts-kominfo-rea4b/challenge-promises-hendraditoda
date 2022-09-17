294c167013b3b7aad79ca112cae919954304471b
Hendradito Dwi A <hendradito21@gmail.com>
2022-09-17 09:35:57 +0700

    Update promise.js
    Penambahan PromiseOutput

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
-const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
-
+const { promiseTheaterIXX, promiseTheaterVGC } = require('./external.js');
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const dataTheaterIXX = await promiseTheaterIXX();
+    const dataTheaterVGC = await promiseTheaterIXX();
+
+    const dataVGC = dataTheaterVGC.filter((res) => res.hasil === emosi);
+    const dataIXX = dataTheaterIXX.filter((res) => res.hasil === emosi);
+
+    return dataVGC.length + dataIXX.length;
+  } catch (error) {}
+};
 
 module.exports = {
   promiseOutput,
